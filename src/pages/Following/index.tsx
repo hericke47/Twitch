beb088ac7571b69c9e87f97d3296f8d58a64c35b
Herick Exterkoetter <herickherick47@gmail.com>
2020-09-08 07:33:54 -0300

    channelList terminado

--- src/pages/Following/index.tsx
+++ src/pages/Following/index.tsx
@@ -6,6 +6,7 @@ import Heading from '../../components/Heading';
 import Title from '../../components/Title';
 import CategoryList from '../../components/CategoryList';
 import StreamList from '../../components/StreamList';
+import ChannelList from '../../components/ChannelList';
 
 import { Wrapper, Container, Main } from './styles';
 
@@ -41,14 +42,14 @@ const Following: React.FC = () => {
         render: () => <Title>Continue Whatching</Title>,
         isTitle: true,
       },
-      { key: 'C3', render: () => <View /> },
+      { key: 'C3', render: () => <StreamList /> },
 
       {
         key: 'OFFLINE_CHANNELS',
         render: () => <Title>Offline Channels</Title>,
         isTitle: true,
       },
-      { key: 'C4', render: () => <View /> },
+      { key: 'C4', render: () => <ChannelList /> },
     ];
 
     // Array que contem apenas os index dos elementos que são titulos

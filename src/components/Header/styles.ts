490259cc76305f951ab3e13af6fd104efa2a02c5
Herick Exterkoetter <herickherick47@gmail.com>
2020-09-08 06:13:40 -0300

    estrutura do header

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,21 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+
+`;
+
+export const Avatar = styled.View`
+
+`;
+
+export const OnlineStatus = styled.View`
+
+`;
+
+export const RightSide = styled.View`
+
+`;
+
+export const Button = styled.View`
+
+`;

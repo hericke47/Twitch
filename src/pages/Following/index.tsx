77c333ca815f5cd975b41c52518058402c035fb6
Herick Exterkoetter <herickherick47@gmail.com>
2020-09-08 06:57:16 -0300

    categorylist terminada

--- src/pages/Following/index.tsx
+++ src/pages/Following/index.tsx
@@ -4,6 +4,7 @@ import { View, FlatList } from 'react-native';
 import Header from '../../components/Header';
 import Heading from '../../components/Heading';
 import Title from '../../components/Title';
+import CategoryList from '../../components/CategoryList';
 
 import { Wrapper, Container, Main } from './styles';
 
@@ -25,7 +26,7 @@ const Following: React.FC = () => {
         render: () => <Title>Followed Catgories</Title>,
         isTitle: true,
       },
-      { key: 'C1', render: () => <View /> },
+      { key: 'C1', render: () => <CategoryList /> },
 
       {
         key: 'LIVE_CHANNELS',
